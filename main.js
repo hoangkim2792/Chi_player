31441ef5cf7c944eabc488347fd39931a36eab30
kim <hoangkim050702@gmail.com>
2022-09-28 17:26:05 +0700

    chốt

--- main.js
+++ main.js
@@ -29,54 +29,47 @@ const app = {
   // config: JSON.parse(localStorage.getItem(PlAYER_STORAGE_KEY)) || {},
   songs: [
     {
-      name: "Click Pow Get Down",
-      singer: "Raftaar x Fortnite",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
-      image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
+      name: "Demons",
+      singer: "imagine dragons",
+      path: "./src/music/demons.mp3",
+      image: "./src/img/demons.jpg"
     },
     {
-      name: "Tu Phir Se Aana",
-      singer: "Raftaar x Salim Merchant x Karma",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
-      image:
-        "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
+      name: "Shut Down",
+      singer: "blackpink",
+      path: "./src/music/shutdown.mp3",
+      image: "./src/img/shutdown.jpeg"
     },
     {
-      name: "Naachne Ka Shaunq",
-      singer: "Raftaar x Brobha V",
-      path:
-        "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
-      image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
+      name: "Pink venom",
+      singer: "blackpink",
+      path: "./src/music/pinkvenom.mp3",
+      image: "./src/img/pinkvenom.jpg"
     },
     {
-      name: "Mantoiyat",
-      singer: "Raftaar x Nawazuddin Siddiqui",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
-      image:
-        "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
+      name: "Know me",
+      singer: "Gemini",
+      path: "./src/music/knowme.mp3",
+      image: "./src/img/knowme.jpg"
     },
     {
-      name: "Aage Chal",
-      singer: "Raftaar",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
-      image:
-        "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
+      name: "We don't talk anymore",
+      singer: "Charlie Puth",
+      path: "./src/music/wedontall.mp3",
+      image: "./src/img/wedontall.jpg"
     },
     {
-      name: "Damn",
-      singer: "Raftaar x kr$na",
-      path:
-        "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
-      image:
-        "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg"
+      name: "Bad liar",
+      singer: "imagine dragons",
+      path: "./src/music/badliar.mp3",
+      image: "./src/img/badliar.jpg"
     },
     {
-      name: "Feeling You",
-      singer: "Raftaar x Harjas",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
-      image:
-        "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
-    }
+      name: "Bad liar",
+      singer: "Jamie Miller",
+      path: "./src/music/wishes.mp3",
+      image: "./src/img/wishes.png"
+    },
   ],
   setConfig: function (key, value) {
     this.config[key] = value;
@@ -117,7 +110,7 @@ const app = {
 
     // Xử lý CD quay / dừng
     // Handle CD spins / stops
-    const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
+    const cdThumbAnimate = cdThumb.animate([{ transform: "" }], {
       duration: 10000, // 10 seconds
       iterations: Infinity
     });
